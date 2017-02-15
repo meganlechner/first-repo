80046f5f6d50411d12c3947ffa616abf8e02c36f
BA201 <BA201@MCBA201-C36.classrooms.stlcc>
2017-02-15 13:27:05 -0600

    adds another line of unhelpful output

--- src/console/NewClass.java
+++ src/console/NewClass.java
@@ -3,7 +3,8 @@ package console;
 
 public class NewClass {
     public static void main(String[] args){
-        System.out.println("Hello Github!");
+        System.out.println("Hello GitHub!");
+        System.out.println("I created a repo on GitHub!");
     }
     
 }
